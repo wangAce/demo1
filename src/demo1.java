ba15119fde6a115687636013a9c6107d0072619e
wangAce <1936698863@qq.com>
2018-07-17 11:22:18 +0800

    Initial commit

---
+++ src/demo1.java
@@ -0,0 +1,16 @@
+public class demo1 {
+
+    // 测试代码 : demo1
+    public static void main(String[] args) {
+
+        System.out.print("1231");
+
+        System.out.println("");
+        int i = 5;
+
+       while (i<10){
+            System.out.println("1231");
+            i++;
+        }
+    }
+}
